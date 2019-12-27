03f96ff6fcb26a3d5e102da348d6801b31a443f2
David Eduardo Rearte <dengue8830@gmail.com>
2019-12-27 14:50:15 -0300

    probando

--- src/utils/schedule.service.ts
+++ src/utils/schedule.service.ts
@@ -10,7 +10,7 @@ const moment = extendMoment(Moment);
  * a ----- c
  *     b ----- d
  * So in order to avoid duplicates we add each
- * range pair overlap just once.ddsss
+ * range pair overlap just once.ddssss
  */
 function thereIsOneWayOverlap(current: ISchedule, next: ISchedule): boolean {
   const currentRange = moment.range(current.start, current.end);
